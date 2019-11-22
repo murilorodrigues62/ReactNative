1fc53549ee118f5c4e49d342c5c1cff6349e4c60
Murilo <murilorodrigues62@gmail.com>
2019-11-22 16:26:32 -0300

    Get request added

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,45 @@
 import React, {Component } from 'react';
 import api from '../services/api';
-import { View, Text} from 'react-native';
+import { View, Text, FlatList, Button} from 'react-native';
 
 export default class Main extends Component{
+    static navigationOptions = {
+        title: "MyApp"
+    };
+
+    state = {
+        /* State é observado pelo react e dispara o render de novo caso haja alteração */
+        docs: []
+    };
+
+    componentDidMount(){
+        /* Metodo executado inicialmente */
+        this.loadDocs();
+    }
+
+    loadDocs = async () => {
+        const response = await api.get("/products");
+
+        const {docs} = response.data;
+
+        this.setState({docs});
+    };
+
+    renderItem = ({item}) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+        </View>
+    );
+
     render(){
         return(
             <View>
-                <Text>Hello React Native</Text>
+                <FlatList 
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem} />                               
+                
             </View>
         );
     }
